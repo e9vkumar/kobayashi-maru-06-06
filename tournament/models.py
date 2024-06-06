fd455db30df8164e666c0a308e0b51be91c3a5ce
vkEnine <vkumar@enine.dev>
2024-06-06 10:19:42 +0530

    task 8-Match Model Created

--- tournament/models.py
+++ tournament/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Team(models.Model):
     country = models.CharField(max_length=3, unique=True)
 
+class Match(models.Model):
+    home_team = models.ForeignKey(Team,on_delete=models.CASCADE)
+    away_team = models.ForeignKey(Team,on_delete=models.CASCADE)
+    date = models.DateField()
+
